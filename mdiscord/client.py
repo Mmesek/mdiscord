1fca2cdc6b17b3ff1a0eb2a86636c08d95f0dfa5
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-04-11 14:51:41 +0200

    Abstracted HTTP parts/requests into separate class

--- mdiscord/client.py
+++ mdiscord/client.py
@@ -9,18 +9,15 @@ Discord API Client.
 
 '''
 
-import asyncio, aiohttp
+import asyncio
 from mdiscord.base_model import Snowflake
-import ujson as json
 from . import types as objects
-
+from .http_client import HTTP_Client
 from .opcodes import Opcodes
-from .endpoints import Endpoints
 
 from .serializer import Deserializer, as_dict
-from .exceptions import BadRequest, RequestError, JsonBadRequest
 
-class Client(Endpoints, Opcodes):
+class WebSocket_Client(HTTP_Client, Opcodes):
     username: str = "[NOT CONNECTED]"
     counters: dict = dict
     latency: float = 0.0
@@ -31,7 +28,6 @@ class Client(Endpoints, Opcodes):
     emoji: dict = dict
     primary_guild: Snowflake = 463433273620824104
     def __init__(self, name: str, cfg: dict, db: object, cache: object, shard: int = 0, total_shards: int = 1):
-        self.token = cfg['DiscordTokens'][name]
         self.username = '[NOT CONNTECTED] ' + name
         self.counters = {"EXECUTED_COMMANDS": 0}
         
@@ -62,125 +58,16 @@ class Client(Endpoints, Opcodes):
         self.primary_guild = cfg[name].get('primary_guild', 463433273620824104)
         
         self.shards = [shard, total_shards]
-        self.lock = {"global": False}
 
         self.decompress = Deserializer()
-        
+        super().__init__(token=cfg['DiscordTokens'][name], user_id=cfg[name].get('user_id'))
         print("\nInitating Bot with token: ", self.token)
-
-    async def _api_call(self, path: str, method: str = "GET", reason: str = None, **kwargs):
-        headers = [("Authorization", f"Bot {self.token}")]
-        if reason:
-            headers.append(("X-Audit-Log-Reason", reason))
-        def remove_None(d):
-            for k, a in d.copy().items():
-                if a is None:
-                    d.pop(k, None)
-                elif type(a) is dict:
-                    d[k] = remove_None(a)
-                elif type(a) is list:
-                    d[k] = [remove_None(i) if type(i) is dict else i for i in a]
-            return d
-        if kwargs.get('json'):
-            kwargs['json'] = as_dict(kwargs['json'])
-            kwargs['json'] = remove_None(kwargs.get('json',{}))
-#        for key, arg in kwargs.get('json',{}).copy().items():
-#            if arg is None:
-#                kwargs['json'].pop(key, None)
-        if kwargs.get('params'):
-            for param in kwargs["params"]:
-                kwargs["params"][param] = str(kwargs["params"][param])
-            for i in ["before", "after", "between"]: #
-                if kwargs["params"].get(i, False) is None: #
-                    kwargs.pop(i) #
-        #TODO: kwargs to json #I think it's handled now?
-        if not kwargs.get('file'):
-            headers.append(("Content-Type", "application/json"))
-            data = None
-        else:
-            data = aiohttp.FormData()
-            data.add_field("payload_json", json.dumps(kwargs["json"]))
-            data.add_field("file", kwargs["file"],
-                filename=kwargs["filename"],
-                content_type="application/octet-stream"
-            )
-            kwargs.pop('json')
-        kwargs.pop('filename', None)
-        kwargs.pop('file', None)
-        res = await self._client_session.request( ##
-            method, objects.BASE_URL + path,
-            data=data, headers=headers, 
-            **kwargs)
-        if not res.ok and res.status != 403: #
-            print(res.status, res.reason, path) #
-        return res #
-
-    async def api_call(self, path: str, method: str = "GET", reason: str = None, **kwargs):
-        try:
-            bucket = path.split('/', 3)[2]
-        except:
-            bucket = None
-
-        if self.lock.get(bucket, False) is False and self.lock.get('global') is False:
-            from .models import HTTP_Response_Codes
-            res = await self._api_call(path, method, reason, **kwargs)
-            if res.status == HTTP_Response_Codes.NO_CONTENT.value:
-                return None
-
-            elif res.status == HTTP_Response_Codes.BAD_REQUEST.value:
-                raise BadRequest(f"[{res.reason}] {await res.text()}", f"[{method}] {path}")
-
-            elif res.status == HTTP_Response_Codes.TOO_MANY_REQUESTS.value:
-                is_global = res.headers.get('X-RateLimit-Global') is True
-                if not is_global:
-                    self.lock[bucket] = True
-                else:
-                    self.lock['global'] = True
-                retry_after = float(res.headers.get('Retry-After', 1))
-                if 'reaction' in path:
-                    retry_after += 0.75
-                await asyncio.sleep(retry_after / 1000 if retry_after > 3 else retry_after)
-                if not is_global:
-                    self.lock[bucket] = False
-                else:
-                    self.lock['global'] = False
-                return await self.api_call(path, method, reason, **kwargs)
-
-            elif res.status >= 500:
-                await asyncio.sleep(1)
-                return await self.api_call(path, method, reason, **kwargs)
-
-            try:
-                r = await res.json()
-            except Exception as ex:
-                raise RequestError(f"Error sending request: [{res.reason}]: [{method}] {path}")
-            
-            if 'message' in r:
-                raise JsonBadRequest(f"[{res.reason}] [{r['code']}] - {r['message']}: [{method}] {path}", r.get('errors','') if 'errors' in r else None)
-            if type(r) is dict:
-                return dict({"_Client": self}, **r)
-            return list(dict({"_Client":self}, **i) for i in r)
-
-        elif 'reaction' in path:
-            # Ratelimit information for them is longer/inconsitent
-            # Still hits 429 hence additional sleep
-            await asyncio.sleep(0.5)
-        await asyncio.sleep(0.75)
-        return await self.api_call(path, method, reason, **kwargs)
 
     async def __aenter__(self):
         self.decompress = Deserializer()
-
-        import platform
-        if 'linux' in platform.system().lower():
-            from aiohttp.resolver import AsyncResolver
-            resolver = AsyncResolver(nameservers=['8.8.8.8', '8.8.4.4'])
-        else:
-            resolver = None
         
-        self._client_session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False, resolver=resolver))#, json_serialize=Encoder().encode)
         gate = await self.get_gateway_bot()
-        self._ws = await self._client_session.ws_connect(f"{gate['url']}?v={self.cfg['Discord']['api_version']}&encoding=json&compress=zlib-stream")
+        self._ws = await self._session.ws_connect(f"{gate['url']}?v={self.cfg['Discord']['api_version']}&encoding=json&compress=zlib-stream")
         return self
 
     async def receive(self):
---
+++ mdiscord/http_client.py
@@ -0,0 +1,127 @@
+from typing import Dict
+from mdiscord.base_model import Snowflake
+from mdiscord.endpoints import Endpoints
+import aiohttp
+
+class HTTP_Client(Endpoints):
+    token: str
+    user_id: Snowflake
+    _session: aiohttp.ClientSession
+    lock: Dict[str, bool]
+    def __init__(self, token=None, user_id=None) -> None:
+        self.token=token
+        self.user_id = user_id
+        
+        import platform
+        if 'linux' in platform.system().lower():
+            from aiohttp.resolver import AsyncResolver
+            resolver = AsyncResolver(nameservers=['8.8.8.8', '8.8.4.4'])
+        else:
+            resolver = None
+        
+        self._session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False, resolver=resolver))#, json_serialize=Encoder().encode)
+        self.lock = {"global": False}
+    
+    def _prepare_payload(self, **kwargs):
+        if 'headers' not in kwargs:
+            kwargs['headers'] = []
+        if self.token:
+            kwargs['headers'].append(("Authorization", f"Bot {self.token}"))
+        if kwargs.get('reason'):
+            kwargs['headers'].append(("X-Audit-Log-Reason", kwargs.pop('reason')))
+        if not kwargs.get('file'):
+            kwargs['headers'].append(("Content-Type", "application/json"))
+        else:
+            kwargs['data'] = aiohttp.FormData()
+            import ujson as json
+            kwargs['data'].add_field("payload_json", json.dumps(kwargs["json"]))
+            kwargs['data'].add_field("file", kwargs["file"],
+                filename=kwargs["filename"],
+                content_type="application/octet-stream"
+            )
+            kwargs.pop('json')
+
+        kwargs = self._serialize(**kwargs)
+        return kwargs
+
+    def _serialize(self, **kwargs):
+        if kwargs.get('json'):
+            from mlib.utils import remove_None
+            from .serializer import as_dict
+            kwargs['json'] = as_dict(kwargs['json'])
+            kwargs['json'] = remove_None(kwargs.get('json',{}))
+        if kwargs.get('params'):
+            for param in kwargs["params"]:
+                kwargs["params"][param] = str(kwargs["params"][param])
+            for i in ["before", "after", "between"]:
+                if kwargs["params"].get(i, False) is None:
+                    kwargs.pop(i)
+        kwargs.pop('filename', None)
+        kwargs.pop('file', None)
+        return kwargs
+
+    async def api_call(self, path: str, method: str, **kwargs):
+        kwargs = self._prepare_payload(**kwargs)
+        return await self._api_call(path, method, **kwargs)
+    
+    async def _api_call(self, path: str, method: str="GET", **kwargs):
+        import asyncio
+        try:
+            bucket = path.split('/', 3)[2]
+        except:
+            bucket = None
+        if not (self.lock.get(bucket, False) is False and self.lock.get('global') is False):
+            if 'reaction' in path:
+                await asyncio.sleep(0.5)
+            await asyncio.sleep(0.75)
+            return await self._api_call(path, method, **kwargs)
+
+        from mdiscord.base_model import BASE_URL
+        async with self._session.request(method, BASE_URL+path, **kwargs) as res:
+            from mdiscord.models import HTTP_Response_Codes
+            from mdiscord.exceptions import BadRequest, RequestError, JsonBadRequest
+
+            try:
+                res.raise_for_status()
+            except Exception as ex:
+                print(ex)
+
+            if res.ok:
+                if res.status == HTTP_Response_Codes.NO_CONTENT.value:
+                    return None
+                try:
+                    r = await res.json()
+                except Exception as ex:
+                    raise RequestError(f"Error sending request: [{res.reason}]: [{method}] {path}")
+
+                if 'message' in r:
+                    raise JsonBadRequest(f"[{res.reason}] [{r['code']}] - {r['message']}: [{method}] {path}", r.get('errors','') if 'errors' in r else None)
+                if type(r) is dict:
+                    return dict({"_Client": self}, **r)
+                return list(dict({"_Client":self}, **i) for i in r)
+            elif res.status == HTTP_Response_Codes.BAD_REQUEST.value:
+                raise BadRequest(f"[{res.reason}] {await res.text()}", f"[{method}] {path}")
+
+            elif res.status == HTTP_Response_Codes.TOO_MANY_REQUESTS.value:
+                is_global = res.headers.get('X-RateLimit-Global') is True
+                if not is_global:
+                    self.lock[bucket] = True
+                else:
+                    self.lock['global'] = True
+                retry_after = float(res.headers.get('Retry-After', 1))
+                if 'reaction' in path:
+                    retry_after += 0.75
+                await asyncio.sleep(retry_after / 1000 if retry_after > 3 else retry_after)
+                if not is_global:
+                    self.lock[bucket] = False
+                else:
+                    self.lock['global'] = False
+                return await self._api_call(path, method, **kwargs)
+            
+            elif res.status >= 500:
+                await asyncio.sleep(1)
+                return await self._api_call(path, method, **kwargs)
+            res.raise_for_status()
+
+    async def close(self):
+        await self._session.close()
