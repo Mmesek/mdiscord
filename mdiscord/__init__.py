0694440f6ca9b7c2ed3fc5a9f2894befcee64f1d
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-06-17 00:18:55 +0200

    Added overwriting function name by event name

--- mdiscord/__init__.py
+++ mdiscord/__init__.py
@@ -12,10 +12,12 @@ from .types import * # noqa: F401
 from .client import WebSocket_Client as Client # noqa: F401
 from .exceptions import * # noqa: F401
 
-def onDispatch(f=None, priority=100):
+def onDispatch(f=None, priority=100, event=None):
     def inner(f):
         from .opcodes import Dispatch
         name = f.__name__.upper()
+        if event:
+            name = event.upper()
         if name not in Dispatch:
             Dispatch[name] = {}
         if priority not in Dispatch[name]:
