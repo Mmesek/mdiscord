4b77b484ca86140945ebc01888ecc160bf89f970
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-09-22 04:05:08 +0200

    Added docstring & optional setting

--- mdiscord/__init__.py
+++ mdiscord/__init__.py
@@ -12,12 +12,30 @@ from .types import * # noqa: F401
 from .websocket import WebSocket_Client as Client # noqa: F401
 from .exceptions import * # noqa: F401
 
-def onDispatch(f=None, priority=100, event=None):
+def onDispatch(f=None, priority: int=100, event: str=None, optional: bool = False):
+    """
+    Decorator to register function as a listener for Event from Dispatch
+    Params
+    ------
+    f:
+        Decorated Function to be registered
+    priorty:
+        Controls priority in case of multiple functions listening for the same event. 
+        Function is appended at the end to current functions with same priority
+    event:
+        Optional Event to which this functions should listen to.
+        Default is same as function name
+    optional:
+        Whether this listener should be excluded from Intent calculation.
+        For example, if execution is optional
+    """
     def inner(f):
         from .opcodes import Dispatch
         name = f.__name__.upper()
         if event:
             name = event.upper()
+        if optional:
+            f._optional = optional
         if name not in Dispatch:
             Dispatch[name] = {}
         if priority not in Dispatch[name]:
