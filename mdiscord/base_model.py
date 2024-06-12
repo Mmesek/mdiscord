eb69fa4a6a5cdf8df316d181ec6e1ea024dd2710
Mmesek <13630781+Mmesek@users.noreply.github.com>
2024-06-12 03:22:57 +0200

    Update DiscordObject to use msgspec.Struct
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- mdiscord/base_model.py
+++ mdiscord/base_model.py
@@ -5,21 +5,24 @@ Discord API
 
 Base Types.
 
-:copyright: (c) 2021 Mmesek
+:copyright: (c) 2021-2024 Mmesek
 
 """
-from __future__ import annotations
-
-from typing import Optional, TYPE_CHECKING, List, Type, TypeVar
+from typing import Optional, TYPE_CHECKING, Type, TypeVar
 from enum import Flag
-from dataclasses import dataclass, is_dataclass, asdict, fields
-from datetime import datetime
+from datetime import datetime, UTC
+import msgspec
 
 from mlib.types import Enum
 from .serializer import as_dict
 
 if TYPE_CHECKING:
     from .websocket import WebSocket_Client as Bot
+else:
+
+    class Bot:
+        pass
+
 
 _T = TypeVar("_T")
 
@@ -61,7 +64,7 @@ class Snowflake(int):
     @property
     def as_date(self):
         ms = (self._value >> 22) + DISCORD_EPOCH
-        return datetime.utcfromtimestamp(ms // 1000.0).replace(microsecond=ms % 1000 * 1000)
+        return datetime.fromtimestamp(ms // 1000.0, UTC).replace(microsecond=ms % 1000 * 1000)
 
     def styled_date(self, style: str = None) -> str:
         return f"<t:{int(self.timestamp/1000.0)}{':'+style if style else ''}>"
@@ -80,9 +83,12 @@ class Events(Enum):
         obj._value_ = len(cls.__members__) + 1
         return obj
 
+    def type(self):
+        return self.func
+
 
 class Flag(Flag):
-    def check(cls, permissions: hex, *values: List[hex]):
+    def check(cls, permissions: hex, *values: list[hex]):
         return all([(permissions & permission) == permission for permission in values])
 
     def current_permissions(cls, permissions: hex):
@@ -93,90 +99,16 @@ class Flag(Flag):
         return current
 
 
-@dataclass
-class DiscordObject:
-    _Client: Optional[Bot] = None
+class DiscordObject(msgspec.Struct, kw_only=True, omit_defaults=True):
+    _Client: Optional[Bot] = msgspec.UNSET
 
-    def __post_init__(self):
-        for field in self.__dict__:
-            if field.startswith("_"):
-                continue
-            __type = self.__annotations__.get(field) or type(self).__bases__[0].__annotations__.get(field)
-            if not __type:
-                _annotations = {}
-                for i in [i.__annotations__ for i in type(self).mro()[:-2]]:
-                    _annotations.update(i)
-                __type = _annotations.get(field)
-            value = getattr(self, field)
-            if value is None or type(value) not in [int, str, bool, type, list, dict]:
-                continue
-            if type(value) is list and value and is_dataclass(value[0]):
-                continue
-            _type = __type.replace("List[", "").replace("]", "").replace("Dict[", "")
-            if "Dict" in __type:
-                k, _type = _type.split(", ")
-            is_basic = _type in ["int", "str", "Snowflake", "bool", "datatime"]
-            types = {"int": int, "bool": bool, "str": str}
-            from . import types as FINAL_TYPES
-
-            _type = types.get(_type, vars(FINAL_TYPES).get(_type))
-            # globals().get(_type))
-            if _type is None:
-                continue
-            elif value is list:
-                # continue
-                self.__setattr__(field, [])
-            elif value is dict:
-                self.__setattr__(field, {})
-            elif "List" in __type:
-                try:
-                    if issubclass(_type, Enum):
-                        self.__setattr__(field, [getattr(_type, i, i) for i in value if i])
-                    else:
-                        self.__setattr__(
-                            field,
-                            [
-                                _type.from_dict(**i if not is_basic and not is_dataclass(i) else i or None)
-                                for i in value or []
-                            ],
-                        )
-                except:
-                    try:
-                        self.__setattr__(
-                            field, [_type.from_dict(**i) if type(i) is not _type else i for i in value or []]
-                        )
-                    except Exception as ex:
-                        self.__setattr__(field, [_type(i) if type(i) is not _type else i for i in value or []])
-            elif _type is datetime:
-                self.__setattr__(field, _type.fromisoformat(value) if value else _type.now())
-            elif "Dict" in __type:
-                # k = types.get(k, vars(FINAL_TYPES).get(k))
-                try:
-                    self.__setattr__(field, {str(_key): _type.from_dict(**_val) for _key, _val in value.items() or {}})
-                except Exception as ex:
-                    self.__setattr__(field, {str(_key): _val for _key, _val in value.items() or {}})
-            else:
-                if type(value) is dict:
-                    self.__setattr__(field, _type.from_dict(**value if value is not None else value or None))
-                elif issubclass(_type, Flag):
-                    _flags = []
-                    for _flag in _type:
-                        if value & _flag.value == 0:
-                            _flags.append(_flag)
-                    self.__setattr__(field, _flags)
-                elif value == 0:
-                    self.__setattr__(field, 0 if _type is not str else "0")
-                else:
-                    self.__setattr__(field, _type(value or (0 if _type is not str else "")))
-
-    # @property
     def as_dict(self):
-        _dict = asdict(self)
+        from .utils import serializer
+
+        _dict = msgspec.to_builtins(self, enc_hook=serializer)
         for field in _dict:
             if field == "_Client":
                 continue
-            # if is_dataclass(_dict.get(field)):
-            #    _dict[field] = asdict(_dict.get(field))
             else:
                 _dict[field] = as_dict(_dict.get(field))
         _dict.pop("_Client")
@@ -184,4 +116,6 @@ class DiscordObject:
 
     @classmethod
     def from_dict(cls, **kwargs):
-        return cls(**{k: v for k, v in kwargs.items() if k in list(dir(cls))})
+        from .utils import deserializer
+
+        return msgspec.convert(kwargs, cls, dec_hook=deserializer)
