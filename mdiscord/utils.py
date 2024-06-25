6d1282113cbe35afecc272b0846bcfb61d67b572
Mmesek <13630781+Mmesek@users.noreply.github.com>
2024-06-25 23:59:11 +0200

    Remove unused dataclass_from_dict function
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- mdiscord/utils.py
+++ mdiscord/utils.py
@@ -22,40 +22,6 @@ log = logging.getLogger("mdiscord")
 log.setLevel(logger.log_level)
 
 
-def dataclass_from_dict(klass, dikt):
-    from dataclasses import fields
-
-    try:
-        types = {"int": int, "bool": bool, "str": str}
-        fieldtypes = {}
-        for f in fields(klass):
-            _type = f.type
-            is_list = False
-            if "List" in f.type:
-                _type = f.type.replace("List[", "").replace("]", "")
-                is_list = True
-            _type = types.get(_type, globals().get(_type))
-            if is_list:
-                fieldtypes[f.name] = (list, _type)
-            else:
-                fieldtypes[f.name] = _type
-        # fieldtypes = {f.name:types.get(f.type, globals().get(f.type)) if 'List' not in f.type else (list, f.type.replace('List[', '').replace(']', '')) for f in fields(klass)}
-        # fieldtypes = {f.name:types.get(f.type, globals().get(f.type)) for f in fields(klass)}
-        _dict = {}
-        for f in dikt:
-            if f not in fieldtypes:
-                continue
-            if type(fieldtypes[f]) is tuple:
-                _dict[f] = [dataclass_from_dict(fieldtypes[f][1], i) for i in dikt[f]]
-            else:
-                _dict[f] = dataclass_from_dict(fieldtypes[f], dikt[f])
-        return klass(**_dict)
-        # return klass(**{f:dataclass_from_dict(fieldtypes[f],dikt[f]) for f in dikt if f in fieldtypes})
-    except Exception as ex:
-        # print(ex)
-        return dikt
-
-
 def Permissions(*permissions):
     def inner(f):
         def wrapped(Client, id, *args, **kwargs):
